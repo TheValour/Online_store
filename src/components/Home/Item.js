a34a780ee601abc87d2845d4fa079744ce13e20d
TheValour <helloworldexe18@gmail.com>
2023-05-15 01:54:25 +0530

    dividing components

---
+++ src/components/Home/Item.js
@@ -0,0 +1,37 @@
+import React, { useState, useContext } from 'react'
+import styles from './ItemContainer.module.css';
+import CartContext from '../context';
+
+
+export default function Item({ product }) {
+    const [items, setItems] = useState(1);
+    const ctx = useContext(CartContext);
+
+    const onChangeHandler = (event, productId) => {
+        setItems(event.target.value);
+    };
+
+    function addTocart(productId) {
+        ctx.setNoCart(pre => pre + Number(items));
+        ctx.setItems(productId, items);
+        console.log(productId);
+    }
+    return (
+        <div key={product.id} className={styles.product}>
+            <img src={product.image} alt={product.name} className={styles.image} />
+            <div className={styles.details}>
+                <h3 className={styles.name}>{product.name}</h3>
+                <p className={styles.price}>{`$ ${product.price}`}</p>
+                <button className={styles.button} onClick={() => addTocart(product.id)}>
+                    Add to Cart
+                </button>
+                <input
+                    type="number"
+                    onChange={onChangeHandler}
+                    className={styles.input}
+                    placeholder='1'
+                />
+            </div>
+        </div>
+    )
+}
--- src/components/Home/ItemContainer.js
+++ src/components/Home/ItemContainer.js
@@ -1,41 +1,14 @@
-import { useContext, useState } from 'react';
 import styles from './ItemContainer.module.css';
-import CartContext from '../context';
+import Item from './Item';
 
 export default function ItemContainer({ meals }) {
-    const [items, setItems] = useState(1);
-    const ctx = useContext(CartContext);
-
-    const onChangeHandler = (event, productId) => {
-        setItems(event.target.value);
-    };
-
-    function addTocart(productId) {
-        ctx.setNoCart(pre => pre + Number(items));
-        ctx.setItems(productId, items);
-        console.log(productId); // Access the productId here
-    }
 
     return (
         <div className={styles.container}>
             {meals.map((product) => (
-                <div key={product.id} className={styles.product}>
-                    <img src={product.image} alt={product.name} className={styles.image} />
-                    <div className={styles.details}>
-                        <h3 className={styles.name}>{product.name}</h3>
-                        <p className={styles.price}>{`$ ${product.price}`}</p>
-                        <button className={styles.button} onClick={() => addTocart(product.id)}>
-                            Add to Cart
-                        </button>
-                        <input
-                            type="number"
-                            onChange={onChangeHandler}
-                            className={styles.input}
-                            placeholder='1'
-                        />
-                    </div>
-                </div>
-            ))}
-        </div>
+                <Item product={product} />
+            ))
+            }
+        </div >
     );
 }
