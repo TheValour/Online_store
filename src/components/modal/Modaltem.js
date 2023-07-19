d7bbe07765507fcfc37f18b75eeec7dff90f5b09
TheValour <helloworldexe18@gmail.com>
2023-07-19 23:41:07 +0530

    adding basic + and - option in Cart modal

--- src/components/modal/Modaltem.js
+++ src/components/modal/Modaltem.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import style from './ModaItem.module.css';
+import CartContext from '../context';
 
 export default function Modaltem({ product, frequency }) {
+    const ctx = useContext(CartContext);
+
+    function incrementHandler() {
+        ctx.setItems(product.id, 1);
+        ctx.setNoCart(pre => pre + 1);
+    }
+    function decrementHandler() {
+        ctx.setItems(product.id, -1);
+        ctx.setNoCart(pre => pre - 1);
+    }
+
     return (
         <div className={style.modalItem}>
             <div>
                 {product.name}<br />
-                <div className={style.price}>$ {product.price}</div>
+                <div className={style.price}>&#8377; {product.price}</div>
+            </div>
+            <div>
+                <button className={style.btn} onClick={incrementHandler}>+</button>
+                <button className={style.btn} onClick={decrementHandler}>-</button>
             </div>
             <div className={style.frequencyContainer}>{+frequency}</div>
         </div>
